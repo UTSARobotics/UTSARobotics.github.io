759cae68b9139d3dca68dba2a28eb815bfb37289
Jackson <urmome37@gmail.com>
2025-10-01 11:52:08 -0500

    basic router

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Test")
+	router := gin.Default()
+	router.Static("/assets", "./web/pub/assets/")
+	router.StaticFile("/", "./web/pub/index.html")
+
+	router.NoRoute(func(c *gin.Context) {
+		c.Redirect(307, "/")
+	})
+
+	// no login page exists yet
+	router.GET("/login", func(c *gin.Context) {
+		c.Redirect(307, "/")
+	})
+
+	// does not exists yet
+	router.GET("/dashboard", func(c *gin.Context) {
+		c.Redirect(307, "/")
+	})
+
+	router.Run(":8080")
 }
